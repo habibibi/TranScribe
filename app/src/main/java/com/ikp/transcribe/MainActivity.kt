f82a0abdda8f89dfb90e54a54e9dd6f310b14381
Muhammad Habibi Husni <habibihusni18@gmail.com>
2024-03-14 09:56:44 +0700

    feat: add bottom nav and header

--- app/src/main/java/com/ikp/transcribe/MainActivity.kt
+++ app/src/main/java/com/ikp/transcribe/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ikp.transcribe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ikp.transcribe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNav : BottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.nav_host)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_transaction, R.id.navigation_scan, R.id.navigation_chart, R.id.navigation_setting
+        ))
+        binding.toolbar.setupWithNavController(navController,appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
     }
 }
