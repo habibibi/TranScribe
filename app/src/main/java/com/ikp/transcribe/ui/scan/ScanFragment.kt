5c0d5287653a921c22d107b669e35a085b7bf087
Muhammad Habibi Husni <habibihusni18@gmail.com>
2024-03-30 19:33:34 +0700

    feat: add image capture and image picker functionality

--- app/src/main/java/com/ikp/transcribe/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/ikp/transcribe/ui/scan/ScanFragment.kt
@@ -1,17 +1,27 @@
 package com.ikp.transcribe.ui.scan
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.ikp.transcribe.R
 import com.ikp.transcribe.databinding.FragmentScanBinding
+import java.io.File
+import java.util.concurrent.Executors
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +39,26 @@ class ScanFragment : Fragment() {
     private var param2: String? = null
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding!!
+
+    private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        result  ->
+            if (result != null){
+                val uriResult : Uri = result
+                val inputstream = requireContext().contentResolver.openInputStream(uriResult)
+                val target = File(requireContext().cacheDir, "tmp.jpeg")
+                if(target.exists().not()) {
+                    target.createNewFile()
+                }
+                inputstream!!.copyTo(target.outputStream())
+                requireView().post {
+                    Navigation.findNavController(requireView())
+                        .navigate(R.id.action_navigation_scan_to_scanConfirmationFragment)
+                }
+            }
+    }
+
+    private lateinit var previewView: PreviewView
+    private lateinit var cameraController: LifecycleCameraController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +78,7 @@ class ScanFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        previewView = binding.scannerCameraView
         hideCamera()
         val requestPermissionLauncher =
             registerForActivityResult(
@@ -68,20 +99,53 @@ class ScanFragment : Fragment() {
                 requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
             }
         }
-
+        binding.captureButton.setOnClickListener {
+            capture()
+        }
+        binding.openImageButton.setOnClickListener{
+            chooseImage()
+        }
     }
 
     private fun startCamera(){
-        val previewView: PreviewView = binding.scannerCameraView
-        val cameraController = LifecycleCameraController(requireContext())
+        cameraController = LifecycleCameraController(requireContext())
         cameraController.bindToLifecycle(this)
         cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
         previewView.controller = cameraController
         previewView.visibility = View.VISIBLE
     }
 
+    private fun capture(){
+        val cacheDir = requireContext().cacheDir
+        val outputFileOptions = OutputFileOptions.Builder(File(cacheDir, "tmp.jpeg")).build()
+        cameraController.takePicture(outputFileOptions, Executors.newSingleThreadExecutor(),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(error: ImageCaptureException)
+                {
+                    Log.e("camera", error.toString())
+                }
+
+                override fun onCaptureStarted() {
+                    super.onCaptureStarted()
+                    requireView().post{
+                        binding.capturedPreview.setImageBitmap(previewView.bitmap)
+                        binding.capturedPreview.visibility = View.VISIBLE
+                    }
+                }
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    requireView().post {
+                        Navigation.findNavController(requireView())
+                            .navigate(R.id.action_navigation_scan_to_scanConfirmationFragment);
+                    }
+                }
+        } )
+    }
+
+    private fun chooseImage(){
+        imagePickerLauncher.launch("image/*")
+    }
+
     private fun hideCamera(){
-        val previewView: PreviewView = binding.scannerCameraView
         previewView.visibility = View.GONE
     }
 
