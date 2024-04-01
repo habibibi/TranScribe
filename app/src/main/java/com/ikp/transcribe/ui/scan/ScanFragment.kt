415b7b123db532aa4f9fdad8da1851027735f5f0
Muhammad Habibi Husni <habibihusni18@gmail.com>
2024-04-01 21:37:59 +0700

    fix: import issue

--- app/src/main/java/com/ikp/transcribe/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/ikp/transcribe/ui/scan/ScanFragment.kt
@@ -3,6 +3,7 @@ package com.ikp.transcribe.ui.scan
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
